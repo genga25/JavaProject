c5234463cf659d776251062baae08a5cee55ee8c
genga25 <gengadevi25@gmail.com>
2022-06-24 21:30:50 +0530

    Done

---
+++ src/org/test/sample.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class sample {
+	public static void main(String[] args) {
+		System.out.println("genga");
+		System.out.println("Yadhu");
+		System.out.println("Rajesh");
+	}
+
+}
